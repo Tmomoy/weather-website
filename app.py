85fece96eea7c7998015fc5391268121b056b3fb
Tmomoy <sam269923@gmail.com>
2026-03-09 16:03:19 +0800

    fix weather

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from taiwan_districts import districts, district_city_map
 
 app = Flask(__name__)
 
-API_KEY = "CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
+API_KEY="CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
+
 
 @app.route("/", methods=["GET","POST"])
 def index():
@@ -14,55 +15,56 @@ def index():
     temps=[]
     times=[]
 
+    # 預設城市
+    search="臺北市"
+
     if request.method=="POST":
+        search=request.form.get("city")
 
-        search=request.form.get("city","").strip()
+    if search in district_city_map:
+        city=district_city_map[search]
+    else:
+        city=search
 
-        if search in district_city_map:
-            city=district_city_map[search]
-        else:
-            city=search
+    url="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
 
-        url="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
+    params={
+        "Authorization":API_KEY,
+        "locationName":city
+    }
 
-        params={
-            "Authorization":API_KEY,
-            "locationName":city
+    r=requests.get(url,params=params)
+
+    data=r.json()
+
+    try:
+
+        location=data["records"]["location"][0]
+
+        weather={
+            "city":search,
+            "wx":location["weatherElement"][0]["time"][0]["parameter"]["parameterName"],
+            "temp":location["weatherElement"][2]["time"][0]["parameter"]["parameterName"],
+            "rain":location["weatherElement"][1]["time"][0]["parameter"]["parameterName"]
         }
 
-        r=requests.get(url,params=params)
-        data=r.json()
+        for i,t in enumerate(location["weatherElement"][0]["time"]):
 
-        try:
+            temp=location["weatherElement"][2]["time"][i]["parameter"]["parameterName"]
+            rain=location["weatherElement"][1]["time"][i]["parameter"]["parameterName"]
 
-            location=data["records"]["location"][0]
+            forecast.append({
+                "time":t["startTime"][5:16],
+                "wx":t["parameter"]["parameterName"],
+                "temp":temp,
+                "rain":rain
+            })
 
-            weather={
-                "city":search,
-                "wx":location["weatherElement"][0]["time"][0]["parameter"]["parameterName"],
-                "rain":location["weatherElement"][1]["time"][0]["parameter"]["parameterName"],
-                "temp":location["weatherElement"][2]["time"][0]["parameter"]["parameterName"]
-            }
+            temps.append(int(temp))
+            times.append(t["startTime"][11:16])
 
-            times_data=location["weatherElement"][0]["time"]
-
-            for i,t in enumerate(times_data):
-
-                temp=location["weatherElement"][2]["time"][i]["parameter"]["parameterName"]
-                rain=location["weatherElement"][1]["time"][i]["parameter"]["parameterName"]
-
-                forecast.append({
-                    "start":t["startTime"][5:16],
-                    "wx":t["parameter"]["parameterName"],
-                    "temp":temp,
-                    "rain":rain
-                })
-
-                temps.append(int(temp))
-                times.append(t["startTime"][11:16])
-
-        except:
-            weather=None
+    except:
+        weather=None
 
     return render_template(
         "index.html",
@@ -73,5 +75,6 @@ def index():
         districts=districts
     )
 
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
