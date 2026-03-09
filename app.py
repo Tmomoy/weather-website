0f9d6aae9e1ef96a46a37c8d4d6684d9c24b8109
Tmomoy <sam269923@gmail.com>
2026-03-09 14:50:16 +0800

    upgrade weather UI

--- app.py
+++ app.py
@@ -1,131 +1,69 @@
 from flask import Flask, render_template, request
 import requests
-import os
-from datetime import datetime
 from taiwan_districts import districts, district_city_map
 
 app = Flask(__name__)
 
-API_KEY = os.environ.get("WEATHER_API_KEY")
+API_KEY = "CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
 
-city_map = {
-
-"台北":"Taipei","臺北":"Taipei",
-"新北":"New Taipei",
-"桃園":"Taoyuan",
-"台中":"Taichung","臺中":"Taichung",
-"台南":"Tainan","臺南":"Tainan",
-"高雄":"Kaohsiung",
-"基隆":"Keelung",
-"新竹":"Hsinchu",
-"苗栗":"Miaoli",
-"彰化":"Changhua",
-"南投":"Nantou",
-"雲林":"Yunlin",
-"嘉義":"Chiayi",
-"屏東":"Pingtung",
-"宜蘭":"Yilan",
-"花蓮":"Hualien",
-"台東":"Taitung",
-"澎湖":"Penghu",
-"金門":"Kinmen",
-"連江":"Lienchiang"
-}
 
 @app.route("/", methods=["GET","POST"])
 def index():
 
     weather=None
-    today=[]
-    week=[]
+    temps=[]
+    times=[]
 
-    lat=request.args.get("lat")
-    lon=request.args.get("lon")
+    if request.method=="POST":
 
-    try:
+        search=request.form.get("city","").strip()
 
-        if lat and lon:
+        display_name=search
 
-            weather_url=f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=zh_tw"
-
-            forecast_url=f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=zh_tw"
-
-            city_name="目前位置"
-
+        # 行政區轉縣市
+        if search in district_city_map:
+            city=district_city_map[search]
         else:
+            city=search
 
-            if request.method=="POST":
-                city=request.form.get("city","").strip()
-            else:
-                city="台北"
+        url="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
 
-            if city in district_city_map:
-                city=district_city_map[city]
+        params={
+            "Authorization":API_KEY,
+            "locationName":city
+        }
 
-            if city in city_map:
-                city_query=city_map[city]
-            else:
-                city_query=city
+        r=requests.get(url,params=params)
 
-            weather_url=f"https://api.openweathermap.org/data/2.5/weather?q={city_query},TW&appid={API_KEY}&units=metric&lang=zh_tw"
+        data=r.json()
 
-            forecast_url=f"https://api.openweathermap.org/data/2.5/forecast?q={city_query},TW&appid={API_KEY}&units=metric&lang=zh_tw"
+        try:
 
-            city_name=city
-
-        weather_data=requests.get(weather_url).json()
-        forecast_data=requests.get(forecast_url).json()
-
-        if str(weather_data.get("cod"))=="200":
+            location=data["records"]["location"][0]
 
             weather={
-                "city":city_name,
-                "temp":round(weather_data["main"]["temp"]),
-                "description":weather_data["weather"][0]["description"],
-                "icon":weather_data["weather"][0]["icon"],
-                "humidity":weather_data["main"]["humidity"]
+                "city":display_name,
+                "wx":location["weatherElement"][0]["time"][0]["parameter"]["parameterName"],
+                "rain":location["weatherElement"][1]["time"][0]["parameter"]["parameterName"],
+                "temp":location["weatherElement"][2]["time"][0]["parameter"]["parameterName"]
             }
 
-            for item in forecast_data["list"][:8]:
+            # 模擬溫度曲線資料
+            temps=[weather["temp"],weather["temp"],weather["temp"]]
+            times=["現在","12小時","24小時"]
 
-                today.append({
-                    "time":item["dt_txt"][11:16],
-                    "temp":round(item["main"]["temp"]),
-                    "icon":item["weather"][0]["icon"],
-                    "rain":round(item.get("pop",0)*100)
-                })
+        except:
 
-            days={}
-
-            for item in forecast_data["list"]:
-
-                date=item["dt_txt"].split(" ")[0]
-
-                if date not in days:
-
-                    days[date]={
-                        "temp":round(item["main"]["temp"]),
-                        "icon":item["weather"][0]["icon"]
-                    }
-
-            for d in list(days.keys())[:7]:
-
-                week.append({
-                    "date":datetime.strptime(d,"%Y-%m-%d").strftime("%m/%d"),
-                    "temp":days[d]["temp"],
-                    "icon":days[d]["icon"]
-                })
-
-    except Exception as e:
-        print(e)
+            weather=None
 
     return render_template(
         "index.html",
         weather=weather,
-        today=today,
-        week=week,
+        temps=temps,
+        times=times,
         districts=districts
     )
+
 
 if __name__=="__main__":
     app.run(debug=True)
