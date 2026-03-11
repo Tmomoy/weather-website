7a0cd091c5a25901163fca18b541f2d4c077f078
Tmomoy <sam269923@gmail.com>
2026-03-11 13:09:10 +0800

    fix weather api to F-D0047-091

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from taiwan_districts import districts, district_city_map
 
 app = Flask(__name__)
 
-API_KEY="CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
+API_KEY = "CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
 
 
 @app.route("/")
@@ -15,99 +15,88 @@ def home():
 @app.route("/weather", methods=["POST"])
 def weather():
 
-    search=request.form.get("city","").strip()
+    search = request.form.get("city", "").strip()
 
     if search in district_city_map:
-        city=district_city_map[search]
+        city = district_city_map[search]
     else:
-        city=search
+        city = search
 
-    city=city.replace("台","臺")
+    city = city.replace("台", "臺")
 
-    # 城市格式修正
-    city_map={
-    "臺北":"臺北市",
-    "新北":"新北市",
-    "桃園":"桃園市",
-    "臺中":"臺中市",
-    "臺南":"臺南市",
-    "高雄":"高雄市",
-    "基隆":"基隆市",
-    "新竹":"新竹市",
-    "嘉義":"嘉義市"
+    # 縣市補全
+    if not city.endswith("市") and not city.endswith("縣"):
+
+        if city in ["臺北","新北","桃園","臺中","臺南","高雄","基隆","新竹","嘉義"]:
+            city += "市"
+        else:
+            city += "縣"
+
+    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-091"
+
+    params = {
+        "Authorization": API_KEY,
+        "locationName": city
     }
 
-    if city in city_map:
-        city=city_map[city]
+    forecast = []
+    temps = []
+    rains = []
+    humidity = []
+    times = []
 
-    elif not city.endswith("市") and not city.endswith("縣"):
-        city+="市"
-
-    url="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
-
-    params={
-        "Authorization":API_KEY,
-        "locationName":city
-    }
-
-    forecast=[]
-    temps=[]
-    rains=[]
-    humidity=[]
-    times=[]
-
-    weather={
-        "city":city,
-        "wx":"查詢不到資料",
-        "temp":"--",
-        "rain":"--"
+    weather = {
+        "city": city,
+        "wx": "查詢不到資料",
+        "temp": "--",
+        "rain": "--"
     }
 
     try:
 
-        r=requests.get(url,params=params)
-        data=r.json()
+        r = requests.get(url, params=params)
+        data = r.json()
 
-        locations=data.get("records",{}).get("location",[])
+        location = data["records"]["locations"][0]["location"][0]
 
-        if not locations:
-            raise Exception("No weather data")
+        elements = location["weatherElement"]
 
-        location=locations[0]
+        temp_data = elements[3]["time"]
+        rain_data = elements[7]["time"]
+        hum_data = elements[1]["time"]
+        wx_data = elements[6]["time"]
 
-        wx_data=location["weatherElement"][0]["time"]
-        rain_data=location["weatherElement"][1]["time"]
-        temp_data=location["weatherElement"][2]["time"]
-
-        weather={
-            "city":city,
-            "wx":wx_data[0]["parameter"]["parameterName"],
-            "temp":temp_data[0]["parameter"]["parameterName"],
-            "rain":rain_data[0]["parameter"]["parameterName"]
+        weather = {
+            "city": city,
+            "wx": wx_data[0]["elementValue"][0]["value"],
+            "temp": temp_data[0]["elementValue"][0]["value"],
+            "rain": rain_data[0]["elementValue"][0]["value"]
         }
 
-        for i in range(len(temp_data)):
+        # 只抓 24 小時 (8筆)
+        for i in range(8):
 
-            t=temp_data[i]["startTime"]
+            t = temp_data[i]["startTime"]
 
-            temp=temp_data[i]["parameter"]["parameterName"]
-            rain=rain_data[i]["parameter"]["parameterName"]
-            wx=wx_data[i]["parameter"]["parameterName"]
+            temp = temp_data[i]["elementValue"][0]["value"]
+            rain = rain_data[i]["elementValue"][0]["value"]
+            hum = hum_data[i]["elementValue"][0]["value"]
 
             forecast.append({
-                "time":t[5:16],
-                "temp":temp,
-                "rain":rain,
-                "wx":wx
+                "time": t[5:10],
+                "temp": temp,
+                "rain": rain,
+                "hum": hum
             })
 
             temps.append(int(temp))
             rains.append(int(rain))
-            humidity.append(60)
+            humidity.append(int(hum))
             times.append(t[11:16])
 
     except Exception as e:
-        print("Weather API error:",e)
+
+        print("Weather API error:", e)
 
     return render_template(
         "result.html",
@@ -120,5 +109,5 @@ def weather():
     )
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
