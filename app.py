ad1632b1b0b2f8c5aa43c619b52bc0e7b5eaaae1
Tmomoy <sam269923@gmail.com>
2026-03-11 09:08:39 +0800

    update html

--- app.py
+++ app.py
@@ -1,108 +1,112 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 from taiwan_districts import districts, district_city_map
 
 app = Flask(__name__)
 
-API_KEY = "CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
+API_KEY="CWA-163D1E42-4393-42FE-8302-6E96BAB2974A"
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/")
+def home():
+    return render_template("index.html", districts=districts)
 
-    weather = None
-    forecast = []
-    temps = []
-    times = []
 
-    search = "臺北市"
+@app.route("/weather",methods=["POST"])
+def weather():
 
-    if request.method == "POST":
-        search = request.form.get("city", "").strip()
+    search=request.form.get("city","").strip()
 
-    # 行政區 → 縣市
     if search in district_city_map:
-        city = district_city_map[search]
+        city=district_city_map[search]
     else:
-        city = search
+        city=search
 
-    city = city.replace("台", "臺")
+    city=city.replace("台","臺")
 
     if not city.endswith("市") and not city.endswith("縣"):
 
         if city in ["臺北","新北","桃園","臺中","臺南","高雄","基隆","新竹","嘉義"]:
-            city += "市"
+            city+="市"
         else:
-            city += "縣"
+            city+="縣"
 
-    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
+    url="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-091"
 
-    params = {
-        "Authorization": API_KEY,
-        "locationName": city
+    params={
+        "Authorization":API_KEY,
+        "locationName":city
     }
+
+    forecast=[]
+    temps=[]
+    rains=[]
+    humidity=[]
+    times=[]
+
+    weather=None
 
     try:
 
-        r = requests.get(url, params=params, timeout=10)
+        r=requests.get(url,params=params)
+        data=r.json()
 
-        data = r.json()
+        location=data["records"]["locations"][0]["location"][0]
 
-        locations = data.get("records", {}).get("location", [])
+        elements=location["weatherElement"]
 
-        if len(locations) == 0:
-            raise Exception("no weather data")
+        temp_data=elements[3]["time"]
+        rain_data=elements[7]["time"]
+        hum_data=elements[1]["time"]
+        wx_data=elements[6]["time"]
 
-        location = locations[0]
-
-        wx = location["weatherElement"][0]["time"][0]["parameter"]["parameterName"]
-        rain = location["weatherElement"][1]["time"][0]["parameter"]["parameterName"]
-        temp = location["weatherElement"][2]["time"][0]["parameter"]["parameterName"]
-
-        weather = {
-            "city": search,
-            "wx": wx,
-            "temp": temp,
-            "rain": rain
+        weather={
+            "city":search,
+            "wx":wx_data[0]["elementValue"][0]["value"],
+            "temp":temp_data[0]["elementValue"][0]["value"],
+            "rain":rain_data[0]["elementValue"][0]["value"]
         }
 
-        times_data = location["weatherElement"][0]["time"]
+        for i in range(7):
 
-        for i, t in enumerate(times_data):
+            t=temp_data[i]["startTime"]
 
-            temp = location["weatherElement"][2]["time"][i]["parameter"]["parameterName"]
-            rain = location["weatherElement"][1]["time"][i]["parameter"]["parameterName"]
+            temp=temp_data[i]["elementValue"][0]["value"]
+            rain=rain_data[i]["elementValue"][0]["value"]
+            hum=hum_data[i]["elementValue"][0]["value"]
 
             forecast.append({
-                "time": t["startTime"][5:16],
-                "wx": t["parameter"]["parameterName"],
-                "temp": temp,
-                "rain": rain
+                "time":t[5:10],
+                "temp":temp,
+                "rain":rain,
+                "hum":hum
             })
 
             temps.append(int(temp))
-            times.append(t["startTime"][11:16])
+            rains.append(int(rain))
+            humidity.append(int(hum))
+            times.append(t[5:10])
 
-    except Exception as e:
+    except:
 
-        print("Weather API error:", e)
-
-        weather = {
-            "city": search,
-            "wx": "查詢不到資料",
-            "temp": "--",
-            "rain": "--"
+        weather={
+            "city":search,
+            "wx":"查詢不到資料",
+            "temp":"--",
+            "rain":"--"
         }
 
     return render_template(
-        "index.html",
+        "result.html",
         weather=weather,
         forecast=forecast,
         temps=temps,
+        rains=rains,
+        humidity=humidity,
         times=times,
         districts=districts
     )
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=10000)
